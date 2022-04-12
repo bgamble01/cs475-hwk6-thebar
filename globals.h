bcbf6e896984339c657eb997ce2cf3f5f86ab521
bgamble01 <73133168+bgamble01@users.noreply.github.com>
2022-04-12 16:59:55 -0700

    Update globals.h

--- globals.h
+++ globals.h
@@ -9,8 +9,19 @@
 #define GLOBALS_H_
 
 int num_threads;	// number of customer threads
-int now_serving;	// customer's ID who is being served
+unsigned int now_serving;	// customer's ID who is being served
 
 //TODO - declare some semaphores
+
+sem_t* cust_arrive;
+sem_t* cust_order;
+sem_t* cust_art;
+sem_t* cust_payed;
+sem_t* cust_left;
+sem_t* bar_ready;
+sem_t* drink_done;
+sem_t* pay_rec;
+
+
 
 #endif /* GLOBALS_H_ */
